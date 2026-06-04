019eeec2345ccb9a2ae780c8b89d2fa379b6b6de
Tayb Marrakchi-Benjaafar <tayeb.marrakchi@gmail.com>
2026-06-04 11:19:54 +0100

    highlight from placeholder script

--- assets/page_to_placeholder.js
+++ assets/page_to_placeholder.js
@@ -1,8 +1,8 @@
-// Use JavaScript to fetch the external file and inject it at the beginning
 fetch("page.html")
   .then((response) => response.text())
   .then((data) => {
     // 'afterbegin' inserts the HTML just inside the element, before its first child
     document.getElementById("content-placeholder").insertAdjacentHTML('afterbegin', data);
+    hljs.highlightAll();
   })
   .catch((error) => console.error("Error loading the HTML:", error));
